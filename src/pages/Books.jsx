cedf47d97379a49b72b4cc748703afe561cd1ea4
Nat Marn <112994197+HorrySheet11@users.noreply.github.com>
2026-03-15 12:29:56 +0800

    feat: divided books into old and new testaments

--- src/pages/Books.jsx
+++ src/pages/Books.jsx
@@ -23,22 +23,40 @@ function Books() {
 
 	return (
 		<div className="transition ease-in-out duration-200 ">
-			<h2 className="text-center">Books</h2>
+			<h2 className="text-center mb-10">Books</h2>
 			{!books ? (
 				<p>Loading...</p>
 			) : (
-				<div className="grid p-10 gap-5 grid-cols-[repeat(auto-fill,minmax(185px,1fr))] grid-rows-auto">
-					{books?.map((book) => (
-						<button
-							type="button"
-							onClick={() => setBook(book)}
-							key={book.id}
-							className="block cursor-pointer w-50 h-25 transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300"
-						>
-							<h3>{book.name}</h3>
-						</button>
-					))}
-				</div>
+				<>
+					<h2 className="w-full text-center ">Old Testament</h2>
+					<div className="grid p-10 gap-5 grid-cols-[repeat(auto-fill,minmax(185px,1fr))] grid-rows-auto">
+						{books?.slice(0, 39).map((book) => (
+							<button
+								type="button"
+								onClick={() => setBook(book)}
+								key={book.id}
+								className="block cursor-pointer w-50 h-25 transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300"
+							>
+								<h3>{book.name}</h3>
+							</button>
+						))}
+					</div>
+					<h2 className="w-full text-center ">
+						New Testament
+					</h2>
+					<div className="grid p-10 gap-5 grid-cols-[repeat(auto-fill,minmax(185px,1fr))] grid-rows-auto">
+						{books?.slice(39, 66).map((book) => (
+							<button
+								type="button"
+								onClick={() => setBook(book)}
+								key={book.id}
+								className="block cursor-pointer w-50 h-25 transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300"
+							>
+								<h3>{book.name}</h3>
+							</button>
+						))}
+					</div>
+				</>
 			)}
 		</div>
 	);
