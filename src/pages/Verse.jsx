62b37239df46416078ba9dcf2123d39450911929
Nat Marn <112994197+HorrySheet11@users.noreply.github.com>
2026-03-16 20:48:48 +0800

    fix: fixed verses failing when only strings are present

--- src/pages/Verse.jsx
+++ src/pages/Verse.jsx
@@ -1,57 +1,28 @@
-import { useContext, useEffect, useRef } from "react";
+import { useContext } from "react";
 import { BibleContext } from "../context/BibleContext";
 
 function Verse() {
-	const { verse, verses } = useContext(BibleContext);
-
-	const ref = useRef(null);
-
-	useEffect(() => {
-		const scrollToPosition = (targetId, offset) => {
-			const target = document.getElementById(targetId);
-			const rect = target.getBoundingClientRect();
-			const scrollTop =
-				window.pageYOffset +
-				(rect.top + rect.height / 2) -
-				window.innerHeight / 2 +
-				offset;
-			window.scrollTo({ top: scrollTop, behavior: "smooth" });
-		};
-		const handleScroll = (targetId) => {
-			if (targetId) {
-				scrollToPosition(targetId, 50);
-			}
-		};
-		if (ref.current) {
-			ref.current.addEventListener("click", () => handleScroll(verse));
-		}
-
-		return () => {
-			if (ref.current) {
-				ref.current.removeEventListener("click", () => handleScroll(verse));
-			}
-		};
-	}, [verse]);
+	const { verses } = useContext(BibleContext);
 
 	return (
-		<div ref={ref} className="pb-[25vh]">
+		<div className="pb-[25vh]">
 			{verses?.map((verse) => (
-				<div key={verse.number} className="px-5 py-3 w-full inline-block after:content-[''] after:w-full after:inline-block " id={verse.number}>
-					<h3 className=" float-left h-full pr-3 font-bold">
-						{verse.number}
-					</h3>
+				<div
+					key={verse.number}
+					className="px-5 py-3 w-full inline-block after:content-[''] after:w-full after:inline-block "
+					id={verse.number}
+				>
+					<h3 className=" float-left h-full pr-3 font-bold">{verse.number}</h3>
 					<h4 className="text-justify">
 						{verse.content.map((content, index) => {
 							return (
 								<>
-									<span key={index}>
+									<span key={content.noteId}>
 										{content.text ? (
-											<span className="italic ">
-												{content.text}
-											</span>
-										) : (
-											<span>{content}</span>
-										)}
+											<span className="italic ">{content.text}</span>
+										) : typeof content === "string" ? (
+											<span>{verse.content[index]}</span>
+										) : null}
 									</span>{" "}
 								</>
 							);
