782c0f1b2bf796d209415862cbd0766976a1513d
Nat Marn <112994197+HorrySheet11@users.noreply.github.com>
2026-03-30 10:43:57 +0800

    feat: added link to source code using react icon
    docs: added link to website

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # A simple react bible app using a [<ins>bible API</ins>](https://github.com/HelloAOLab/bible-api)
 
+Check out the app [here](https://bible-app-pi-seven.vercel.app/)
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,16 @@
-import Breadcrumb from './Breadcrumb';
+import { TbSourceCode } from "react-icons/tb";
+import Breadcrumb from "./Breadcrumb";
 
-function Header(){
-  return(
-    <div className='z-100 mb-10 sticky top-0 left-0 bg-linear-to-r from-cyan-500 to-blue-500 w-full h-20 flex flex-row justify-around items-center'>
-      <h1>The Horry Bible</h1>
-      <Breadcrumb />
-    </div>
-  )
+function Header() {
+	return (
+		<div className="z-100 mb-10 sticky top-0 left-0 bg-linear-to-r from-cyan-500 to-blue-500 w-full h-20 flex flex-row justify-around items-center">
+			<div className="flex flex-row gap-5">
+				<h1>The Horry Bible</h1>
+        <a href="https://github.com/HorrySheet11/bible_app" target="_blank" rel="noopener noreferrer" className="h-full"><TbSourceCode size={50}/></a>
+			</div>
+			<Breadcrumb />
+		</div>
+	);
 }
 
 export default Header;
